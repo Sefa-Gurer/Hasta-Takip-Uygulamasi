65f4bc14ce05086d6c1f103963b7845b4661c845
Sefa-Gurer <154366174+Sefa-Gurer@users.noreply.github.com>
2023-12-20 21:29:44 +0300

    kullanıcı sifre sorgulama

--- DEnEme/DEnEme/Form1.Designer.cs
+++ DEnEme/DEnEme/Form1.Designer.cs
@@ -32,9 +32,9 @@ namespace DEnEme
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.kullanici_adi = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sifre = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -66,13 +66,13 @@ namespace DEnEme
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
-            // textBox2
+            // kullanici_adi
             // 
-            this.textBox2.Location = new System.Drawing.Point(85, 322);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(163, 22);
-            this.textBox2.TabIndex = 4;
+            this.kullanici_adi.Location = new System.Drawing.Point(85, 322);
+            this.kullanici_adi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.kullanici_adi.Name = "kullanici_adi";
+            this.kullanici_adi.Size = new System.Drawing.Size(163, 22);
+            this.kullanici_adi.TabIndex = 4;
             // 
             // button1
             // 
@@ -87,13 +87,13 @@ namespace DEnEme
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // sifre
             // 
-            this.textBox1.Location = new System.Drawing.Point(85, 383);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(163, 22);
-            this.textBox1.TabIndex = 7;
+            this.sifre.Location = new System.Drawing.Point(85, 383);
+            this.sifre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.sifre.Name = "sifre";
+            this.sifre.Size = new System.Drawing.Size(163, 22);
+            this.sifre.TabIndex = 7;
             // 
             // label1
             // 
@@ -122,12 +122,12 @@ namespace DEnEme
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Font = new System.Drawing.Font("Lucida Handwriting", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
             this.label3.Location = new System.Drawing.Point(12, 133);
             this.label3.MaximumSize = new System.Drawing.Size(300, 300);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(274, 29);
+            this.label3.Size = new System.Drawing.Size(271, 28);
             this.label3.TabIndex = 10;
             this.label3.Text = "YAŞAM EVDE SAĞLIK";
             // 
@@ -141,9 +141,9 @@ namespace DEnEme
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sifre);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.kullanici_adi);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -160,9 +160,9 @@ namespace DEnEme
         #endregion
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox kullanici_adi;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox sifre;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
--- DEnEme/DEnEme/Form1.cs
+++ DEnEme/DEnEme/Form1.cs
@@ -7,16 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DEnEme
 {
     public partial class Form1 : Form
-    {
+    {    
         public Form1()
         {
             InitializeComponent();
         }
-
+        SqlConnection veri_tabani = new SqlConnection(@"Server = SGURER\SQLEXPRESS;Database=HastaTakip; Trusted_Connection=True");
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -24,7 +25,26 @@ namespace DEnEme
 
         private void button1_Click(object sender, EventArgs e)
         {
+            veri_tabani.Open();
+            
+            int girilen_id= Convert.ToInt32(kullanici_adi.Text);
+            string girilen_sifre = (sifre.Text);
 
+            SqlCommand gercek_sifre = new SqlCommand("Select Sisteme_Giris_Bilgileri.Sifre from Sisteme_Giris_Bilgileri Where Personel=@prmPersonel", veri_tabani);
+            gercek_sifre.Parameters.AddWithValue("@prmPersonel", girilen_id);
+            var sonuc = gercek_sifre.ExecuteScalar();
+
+            // Eğer sonuç null değilse ve girilen_sifre ile eşleşiyorsa
+            if (sonuc != null && girilen_sifre == sonuc.ToString())
+            {
+                // Doğru şifre
+                MessageBox.Show("Şifre doğru!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Şifre yanlış!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            veri_tabani.Close();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
