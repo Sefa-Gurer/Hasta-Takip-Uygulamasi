2efcb03eb2f8f143157226b0c6f134ab1eb00de3
Sefa-Gurer <154366174+Sefa-Gurer@users.noreply.github.com>
2023-12-24 22:16:39 +0300

    hasta ekleme ekranı tamamlandı.(kan grubu sorunu hariç)

--- DEnEme/DEnEme/Form3.Designer.cs
+++ DEnEme/DEnEme/Form3.Designer.cs
@@ -37,40 +37,48 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 961F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 305F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 985F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 281F));
             this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 2, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 29.98944F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70.01056F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1618, 947);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1618, 840);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.SkyBlue;
             this.button1.BackgroundImage = global::DEnEme.Properties.Resources.left_arrows;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Location = new System.Drawing.Point(3, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(346, 278);
+            this.button1.Size = new System.Drawing.Size(346, 247);
             this.button1.TabIndex = 3;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView1
@@ -79,19 +87,20 @@
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(355, 287);
+            this.dataGridView1.Location = new System.Drawing.Point(355, 254);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
             this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(955, 657);
+            this.dataGridView1.Size = new System.Drawing.Size(979, 583);
             this.dataGridView1.TabIndex = 4;
             this.dataGridView1.TabStop = false;
             // 
             // tableLayoutPanel2
             // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.tableLayoutPanel2.ColumnCount = 4;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 78F));
@@ -108,7 +117,7 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.28571F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.71428F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 67F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(955, 278);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(979, 245);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // textBox1
@@ -116,9 +125,9 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(3, 213);
+            this.textBox1.Location = new System.Drawing.Point(3, 180);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(419, 60);
+            this.textBox1.Size = new System.Drawing.Size(443, 60);
             this.textBox1.TabIndex = 0;
             // 
             // button2
@@ -126,9 +135,10 @@
             this.button2.BackgroundImage = global::DEnEme.Properties.Resources.check;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(428, 213);
+            this.button2.Location = new System.Drawing.Point(452, 179);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(72, 62);
+            this.button2.Size = new System.Drawing.Size(72, 64);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -136,11 +146,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(3, 156);
+            this.label1.Location = new System.Drawing.Point(3, 132);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(419, 54);
+            this.label1.Size = new System.Drawing.Size(443, 45);
             this.label1.TabIndex = 4;
             this.label1.Text = "Hasta T.C. Giriniz";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -151,7 +162,7 @@
             this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button3.ForeColor = System.Drawing.Color.Snow;
-            this.button3.Location = new System.Drawing.Point(506, 213);
+            this.button3.Location = new System.Drawing.Point(530, 180);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(186, 62);
             this.button3.TabIndex = 2;
@@ -159,11 +170,40 @@
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(1340, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(275, 245);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.BackgroundImage = global::DEnEme.Properties.Resources.hospital;
+            this.tableLayoutPanel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(269, 239);
+            this.tableLayoutPanel4.TabIndex = 0;
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1618, 947);
+            this.ClientSize = new System.Drawing.Size(1618, 840);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form3";
             this.Text = "Yaşam Evde Sağlık";
@@ -172,6 +212,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -186,5 +227,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
     }
 }
--- DEnEme/DEnEme/Form4.cs
+++ DEnEme/DEnEme/Form4.cs
@@ -9,6 +9,7 @@ using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace DEnEme
 {
@@ -23,7 +24,15 @@ namespace DEnEme
         SqlConnection veri_tabani = new SqlConnection(@"Server = SGURER\SQLEXPRESS;Database=HastaTakip; Trusted_Connection=True");
         private void Form4_Load(object sender, EventArgs e)
         {
-
+            dateTimePicker1.MaxDate = DateTime.Today;
+            comboBox1.Items.Add("0-");
+            comboBox1.Items.Add("0+");
+            comboBox1.Items.Add("A-");
+            comboBox1.Items.Add("A+");
+            comboBox1.Items.Add("B-");
+            comboBox1.Items.Add("B+");
+            comboBox1.Items.Add("AB-");
+            comboBox1.Items.Add("AB+");
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -31,47 +40,100 @@ namespace DEnEme
             this.Hide(); // Form4'ü gizle
             form2.Show();
         }
+
         private void button2_Click(object sender, EventArgs e)
         {
-            string textbox1 = (textBox1.Text.Trim());
-            string girilen_ad = (textBox2.Text.Trim());
-            string girilen_soyad = (textBox3.Text.Trim());
-            string girilen_mail = (textBox4.Text.Trim());
-            string textbox2 = (textBox5.Text.Trim());
-            string girilen_dogum_gunu = (textBox6.Text.Trim());
-            string girilen_kan_grubu = (textBox7.Text.Trim());
-
-            if (textbox1 == "" || girilen_ad == "" || girilen_soyad == "" || textbox2 == "" || girilen_dogum_gunu == "" || girilen_kan_grubu == "")
+            try
             {
-                MessageBox.Show("Doldurulması zorunlu alanlar boş", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                if (girilen_mail != "" && !girilen_mail.Contains("@"))
+                veri_tabani.Open();
+                string textbox1 = (textBox1.Text.Trim());
+                string girilen_ad = (textBox2.Text.Trim());
+                string girilen_soyad = (textBox3.Text.Trim());
+                string girilen_mail = (textBox4.Text.Trim());
+                string textbox2 = (textBox5.Text.Trim());
+                DateTime girilen_dogum_gunu = dateTimePicker1.Value;
+                //string girilen_dogum_gunu = (textBox6.Text.Trim());
+                //|| girilen_dogum_gunu == ""
+                //string girilen_kan_grubu = (textBox7.Text.Trim());
+                //|| girilen_kan_grubu == ""
+                if (comboBox1.SelectedIndex == -1)
                 {
-                    MessageBox.Show("Geçerli bir mail giriniz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Lütfen kan grubu girin.");
                 }
                 else
                 {
-                    Int64 girilen_tc = Convert.ToInt64(textbox1);
-                    Int64 girilen_telefon = Convert.ToInt64(textbox2);
-                    if (girilen_tc < 99999999999 && girilen_tc > 10000000000)
+                    string girilen_kan_grubu = comboBox1.SelectedItem.ToString();
+                    if (textbox1 == "" || girilen_ad == "" || girilen_soyad == "" || textbox2 == "")
                     {
-                        if (girilen_telefon < 5999999999 && girilen_telefon > 5000000000)
-                        {
-                            MessageBox.Show("tc ve telefon doğru", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
-                        else
-                        {
-                            MessageBox.Show("Geçerli bir telefon numarası giriniz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
+                        MessageBox.Show("Doldurulması zorunlu alanlar boş", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                     else
                     {
-                        MessageBox.Show("Geçerli bir T.C. giriniz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (girilen_mail != "" && !girilen_mail.Contains("@"))
+                        {
+                            MessageBox.Show("Geçerli bir mail giriniz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            if (Int64.TryParse(textbox1, out Int64 girilen_tc) && Int64.TryParse(textbox2, out Int64 girilen_telefon))
+                            {
+                                if (girilen_tc < 99999999999 && girilen_tc > 10000000000)
+                                {
+                                    if (girilen_telefon < 5999999999 && girilen_telefon > 5000000000)
+                                    {
+                                        using (SqlCommand cmd = new SqlCommand("INSERT INTO Hastalar(Hasta_tc, Hasta_Ad, Hasta_Soyad, Hasta_Email, Hasta_Tel, Hasta_Dogum_Tarihi, Kan_Grubu) VALUES(@tc, @ad, @soyad, @mail, @telefon,@dogum_tarihi, @kan_grubu)", veri_tabani))
+                                        {
+                                            // Parametreleri ekleyin
+                                            cmd.Parameters.AddWithValue("@tc", girilen_tc);
+                                            cmd.Parameters.AddWithValue("@ad", girilen_ad);
+                                            cmd.Parameters.AddWithValue("@soyad", girilen_soyad);
+                                            cmd.Parameters.AddWithValue("@mail", girilen_mail);
+                                            cmd.Parameters.AddWithValue("@telefon", girilen_telefon);
+                                            cmd.Parameters.AddWithValue("@dogum_tarihi", girilen_dogum_gunu);
+                                            cmd.Parameters.AddWithValue("@kan_grubu", girilen_kan_grubu);
+
+                                            // Sorguyu çalıştırın
+                                            int affectedRows = cmd.ExecuteNonQuery();
+                                            if (affectedRows > 0)
+                                            {
+                                                MessageBox.Show("Hasta başarıyla eklendi.");
+                                                textBox1.Text = string.Empty;
+                                                textBox2.Text = string.Empty;
+                                                textBox3.Text = string.Empty;
+                                                textBox4.Text = string.Empty;
+                                                textBox5.Text = string.Empty;
+                                                dateTimePicker1.Value = DateTime.Now.Date;
+                                                comboBox1.SelectedIndex = -1; // ComboBox'ı sıfırla
+                                            }
+                                            else
+                                            {
+                                                MessageBox.Show("Hasta eklenirken bir hata oluştu.");
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Geçerli bir telefon numarası giriniz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    }
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Geçerli bir T.C. giriniz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Geçerli bir T.C. ve telefon numarası giriniz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
                     }
                 }
             }
+            catch
+            {
+                MessageBox.Show("Hay Aksi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            veri_tabani.Close();
         }
     }
 }
